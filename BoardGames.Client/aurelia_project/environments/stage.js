f0c97a875488f34b4d18cf86574ba0352e7c30cb
Miroslav Popovic <miroslav.popovic@gmail.com>
2018-08-30 22:20:26 +0200

    Fix ASP.NET Core version and add new tokens

--- BoardGames.Client/aurelia_project/environments/stage.js
+++ BoardGames.Client/aurelia_project/environments/stage.js
@@ -2,7 +2,7 @@ export default {
   apiUrl: 'https://localhost:44304/',
   // Use https://localhost:44304/get-token
   // Of course, this would not be hard coded in real app!!!
-  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb2NrZXJ0YWxrIiwianRpIjoiYTFkNTJiMDktZDkzZi00YmRhLWFmZTgtYjBhNzA5ZjdmMDViIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE1Mjg5MjEwNzksImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQzMDQiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQ0MzA0In0.GcEp8AF4pU4NDiEVzQpnWac3Ppw2NMyNiCzFRtyWWv4', 
+  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb2NrZXJ0YWxrIiwianRpIjoiODU3OTkxY2MtMjRhNy00OWIxLWE5NjktMWZlYzZlZTgyYWQyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE1MzgyNTIxMTUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQzMDQiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQ0MzA0In0.Ac6cWWdo-gFvIzXxRxb9BupVz-iX_n2GlOWY96ZVRu4', 
   debug: true,
   testing: false
 };
