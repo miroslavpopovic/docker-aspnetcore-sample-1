f24843fab8ae046f9b379d2716b28fdc9e8fe63a
Miroslav Popovic <miroslav.popovic@gmail.com>
2018-09-20 23:52:16 +0200

    Update expiring tokens

--- BoardGames.Client/aurelia_project/environments/prod.js
+++ BoardGames.Client/aurelia_project/environments/prod.js
@@ -2,7 +2,7 @@ export default {
   apiUrl: 'http://localhost:5110/',
   // Use https://localhost:44304/get-token
   // Of course, this would not be hard coded in real app!!!
-  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb2NrZXJ0YWxrIiwianRpIjoiODU3OTkxY2MtMjRhNy00OWIxLWE5NjktMWZlYzZlZTgyYWQyIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE1MzgyNTIxMTUsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQzMDQiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQ0MzA0In0.Ac6cWWdo-gFvIzXxRxb9BupVz-iX_n2GlOWY96ZVRu4', 
+  apiToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkb2NrZXJ0YWxrIiwianRpIjoiY2Y1Yjk4ZjEtZTFiMS00M2EyLWEwMzUtNTM0YzlmNTQ2ODBjIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9yb2xlIjoiYWRtaW4iLCJleHAiOjE1NDAwNzE1ODEsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NDQzMDQiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQ0MzA0In0.Vn6wkirOWoZlfm37FTODbfeVJ960VOdelbETyD8PpyI', 
   debug: false,
   testing: false
 };
